e1c6b358ed6cab6f3b1c4bd68692310179421b39
Lewis Baston <lbaston@viioni.com>
2025-05-09 14:13:19 +0100

    Add new function to generate from string

--- zpl_test.go
+++ zpl_test.go
@@ -72,6 +72,63 @@ func TestLabelGenerationFromFile(t *testing.T) {
 
 }
 
+func TestLabelGenerationFromString(t *testing.T) {
+	test_zpl := "labels/test/test.zpl"
+	expected_zpl := "labels/test/match.zpl"
+	unexpected_zpl := "labels/test/no_match.zpl"
+
+	expected_content, err := os.ReadFile(expected_zpl)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	unexpected_content, err := os.ReadFile(unexpected_zpl)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	test_file, err := os.ReadFile(test_zpl)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	details := &LabelDetails{
+		DistributorCode:   "UK/Ire",
+		FontSize:          "80",
+		TitleLines:        "1",
+		ShortDescription:  "TEST LABEL",
+		CaseQty:           "6",
+		ProductSizeOption: "200",
+		UnitOfMeasure:     "g",
+		UBNCode:           "UBN123456",
+		EPSCode:           "EPS123456",
+		Outer1DBarcode:    "00112233445566",
+		LotNo:             "123",
+		ExpiryDate:        "12/12/24",
+	}
+
+	detailsMap := GenerateDetailMap(*details)
+	generated_content, err := GenerateLabelString(string(test_file), detailsMap)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if _, _, ok := compareZPLStrings(generated_content, string(unexpected_content)); ok == true {
+		t.Logf("generated label matches unexpected content: expected:\n %s \n got:\n %s \n", strings.TrimSpace(string(unexpected_content)), strings.TrimSpace(generated_content))
+		t.Fail()
+	}
+
+	if exp_line, gen_line, ok := compareZPLStrings(generated_content, string(expected_content)); ok == false {
+		t.Logf("generated label does not match expected content: expected:\n\n\t%s\n\ngot:\n\n\t%s\n\n", []byte(exp_line), []byte(gen_line))
+		t.FailNow()
+	}
+
+}
+
 func compareZPLStrings(gen, exp string) (string, string, bool) {
 	gen_s := strings.TrimSpace(gen)
 	exp_s := strings.TrimSpace(exp)
